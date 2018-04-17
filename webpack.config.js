1206752fadd0637f18aed94ed2c363f9376890a7
Ian Cullianne <ian.cullinane@gmail.com>
2018-04-17 08:59:25 -0400

    add build and management files

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.jsx',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './',
+    hot: true
+  },
+  resolve: {
+    modules: [    
+      path.resolve('./app'),
+      path.resolve('./node_modules')
+    ],
+    extensions: ['.js', '.jsx']
+  },
+  module:{
+    loaders:[{
+        test:/\.(s*)css$/,
+        // the are used in reverse order, output of sass-loader->css-loader->
+        // stlye->loader injects it into the html
+        use:['style-loader','css-loader', 'sass-loader']
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            query: {
+              // If you set something here, also set it in .babelrc
+              presets: ['es2016', 'react'],
+              plugins: ['transform-class-properties']
+          }
+        }
+      }]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Pesto',
+      template: './app/index-template.html',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  }
+};
